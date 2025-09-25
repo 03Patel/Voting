78d16b91e13a7e20b0084f8b2c7a9a0a10fcc9e3
Ganesh  ji patel <ganeshjipatel108@gmail.com>
2025-09-25 20:11:34 +0530

    f0

--- backend/server.js
+++ backend/server.js
@@ -16,5 +16,6 @@ mongoose.connect(process.env.MONGO_URI, {
 }).then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
+app.get("/", (req, res) => res.send("API is running..."));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
